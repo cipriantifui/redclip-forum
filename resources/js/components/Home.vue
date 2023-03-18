b5bce9282913ca5afea6733b78f0b41889e6d2af
Ciprian <ciprian.tifui92@gmail.com>
2023-03-18 11:52:20 +0200

    refactoring post card

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -2,36 +2,11 @@
     <div class="pb-5">
         <div class="row">
             <div class="col-12">
-                <div class="card post-card px-3 py-1 mb-2" v-for="post in posts">
-                    <div class="post-user">{{post.user === null ? 'anonymous' + post.uid : post.user.name}}
-                        <timeago :datetime="post.created_at"></timeago>
-                    </div>
-                    <h4 style="color: #1b1e21">{{ post.title }}</h4>
-                    <span class="badge badge-danger topic-badge">{{ post.topic.title }}</span>
-                    <p class="content pt-2" v-if="post.content">
-                        {{post.content}}
-                    </p>
-                    <p class="content pt-2" v-if="post.url_image">
-                        <img :src="post.url_image" :alt="post.title">
-                    </p>
-                    <p class="content pt-2" v-if="post.url_video">
-                        <iframe
-                            :src="post.url_video"
-                            width="100%"
-                            height="100%"
-                            autoplay="false"
-                            frameborder="0">
-                        </iframe>
-                    </p>
-                    <p class="post-actions">
-                        <span @click="savePostVote(post.id)"><i class="fa fa-thumbs-up"></i> {{post.votes_count}} UpVotes </span>
-                        <span><router-link :to="{ name: 'post-details', params: {post_id: post.id} }"
-                                           class="nav-link"><i class="fa fa-comment"></i> {{post.comments_count}} Comments</router-link></span>
-                    </p>
-                </div>
+                <post-card :post="post" v-for="post in posts" :key="post.id"></post-card>
 
                 <button type="button" class="btn btn-secondary" v-if="paginate.next_page_url"
-                        @click="getPosts(paginate.current_page+1)">View-more
+                        @click="getPosts(paginate.current_page + 1)">
+                    View-more
                 </button>
             </div>
         </div>
@@ -39,8 +14,11 @@
 </template>
 
 <script>
+    import PostCard from "./Post/PostCard.vue";
+
     export default {
         name: "Home.vue",
+        components: {PostCard},
         data() {
             return {
                 posts: {},
@@ -60,55 +38,11 @@
                         this.paginate = response.data;
                     }).catch(error => {
                 });
-            },
-
-            savePostVote(postId) {
-                let url = this.$store.state.isLoggedIn ? '/api/auth/post-vote/create' : '/api/post-vote/create';
-
-                this.axios.post(url, {
-                    post_id: postId,
-                    uid: this.$store.state.isLoggedIn ? null : this.$uoid,
-                }).then(response => {
-                    var foundIndex = this.posts.findIndex(x => x.id === postId);
-                    var votes = this.posts[foundIndex].votes_count;
-                    this.posts[foundIndex].votes_count = response.data.voteUp ? (votes+1) : (votes-1);
-                }).catch(error => {
-                    if (error.response) {
-                        if (error.response.data.errors.error) {
-                            this.$toaster.error(error.response.data.errors.error)
-                        } else {
-                            this.$toaster.error(error.response.data.errors.error)
-                        }
-                    }
-                });
             }
         }
     }
 </script>
 
 <style scoped>
-    .post-card .post-user {
-        color: #6c757d;
-        border-bottom: 2px dashed #ccc;
-        margin-bottom: 5px;
-    }
 
-    .post-actions span {
-        font-weight: bold;
-        cursor: pointer;
-    }
-
-    .post-actions span:hover {
-        font-size: 15px;
-        color: #1b1e21;
-    }
-
-    .post-actions span a {
-        display: inline;
-        color: #585858;
-    }
-
-    .topic-badge {
-        width: min-content;
-    }
 </style>
